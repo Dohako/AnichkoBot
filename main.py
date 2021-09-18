038d695c28237c3c282f3b39f10ab45bc740974c
dohako <dohako@inbox.ru>
2021-09-18 17:45:05 +0300

    created main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+This script will be place for all proccess to start
+
+Main proccesses:
+1. Telegram bot
+2. Continuous git checking
+3. Fetching updates from git, if available
+4. Arduino control
+5. Camera control
+6. Audio control
+7. Microphone control
+"""
