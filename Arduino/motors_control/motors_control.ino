567287b66917507d52d4334e6d25f31073f5567e
dohako <dohako@inbox.ru>
2022-01-22 18:48:57 +0300

    Motor control version 1

--- Arduino/motors_control/motors_control.ino
+++ Arduino/motors_control/motors_control.ino
@@ -1,29 +1,41 @@
-int incomingByte = 0;
+#define SPEED_1      5
+#define DIR_1        4
 
-void show_one(){
-  Serial.println("You entered 0");
+#define SPEED_2      6
+#define DIR_2        7
+
+void move_forward(int motor_speed) {
+  digitalWrite(DIR_1, LOW);
+  analogWrite(SPEED_1, motor_speed);
+  digitalWrite(DIR_2, LOW);
+  analogWrite(SPEED_2, motor_speed);
+  Serial.println("Moving forward");
+}
+
+void stop_moving() {
+  digitalWrite(DIR_1, LOW);
+  analogWrite(SPEED_1, 0);
+  digitalWrite(DIR_2, LOW);
+  analogWrite(SPEED_2, 0);
+  Serial.println("Stoping");
 }
 
 void setup() {
   Serial.begin(9600);
+  for (int i = 4; i < 8; i++) {
+    pinMode(i, OUTPUT);
+  }
 }
 
 void loop() {
-  if (Serial.available() > 0)
-  {
-    incomingByte = Serial.read();
-
-    if (incomingByte == '0') {
-      show_one();
+  if (Serial.available() > 0) {
+    String a = Serial.readString();
+    Serial.print(a);
+    if (a == "stop\n"){
+      stop_moving();
     }
-    else if (incomingByte == '1') {
-      Serial.println("You entered 1");
-    }
-    else if (incomingByte == '2') {
-      Serial.println("You entered 2");
-    }
-    else if (incomingByte == '3') {
-      Serial.println("You entered 3");
+    else if (a == "forward\n") {
+      move_forward(100);
     }
   }
 }
