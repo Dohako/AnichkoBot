2fab1ad7189679092c78cbe732d9da3dec75a179
dohako <dohako@inbox.ru>
2022-01-22 16:48:51 +0300

    testing git

--- Raspberry/rpi_cicd/check_git.py
+++ Raspberry/rpi_cicd/check_git.py
@@ -6,5 +6,6 @@ import subprocess
 
 subprocess.run("sudo /mnt/c/Projects/d_sh_handler/autorun/repo_watcher/git-repo-watcher -d /mnt/c/Projects/d_sh_handler", 
 check=True, text=True, shell=True)
+print(123)
 # subprocess.run("dir", 
 # check=True, text=True, shell=True)
